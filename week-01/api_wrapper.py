1493111c293d549f04fe0e15a48eafa89e16b56d
Aziz ur Rehman <chaziz123bwp@gmail.com>
2026-08-15 10:10:58 +0500

    Add Task 5: reusable API wrapper with retry, timeout, and error handling

---
+++ week-01/api_wrapper.py
@@ -0,0 +1,44 @@
+import os
+import openai
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+class APIWrapper:
+    def __init__(self,model='gpt-4o-mini'):
+        self.client=OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+        self.model=model
+
+    def send_message(self,prompt):
+        attempt=1
+        max_attempts=3
+        while attempt <= max_attempts:
+            try:
+                response=self.client.chat.completions.create(
+                 model=self.model,
+                    messages=[{'role':'user','content':prompt}],
+                    timeout=10
+                )
+                print(f'Tokens used \n:Input Tokens: {response.usage.prompt_tokens} ,\nOutput Tokens: {response.usage.completion_tokens} ,\nTotal tokens: {response.usage.total_tokens}')
+                return response
+            except openai.AuthenticationError:
+                print('Invalid API key,check your API key')
+                return None
+            except openai.RateLimitError:
+                print(f'Rate limit hit on attempt {attempt}, retrying...')
+                attempt+=1
+            except Exception as e:
+                print(f'Attempt {attempt} failed: {e}')
+                attempt+=1
+
+        print('All attempts failed')
+        return None
+
+if __name__=='__main__':
+    wrapper=APIWrapper()
+    result= wrapper.send_message('Tell me about most famous food in Bahawalpur in one sentence.')
+    if result is not None:
+        print(result.choices[0].message.content)
+    else:
+        print('No response recieved')
